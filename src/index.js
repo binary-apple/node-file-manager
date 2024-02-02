8a546f55970002b6aae68d1e372cedaef0269fca
Anna Perevedentseva <anna.pereved@gmail.com>
2024-02-02 16:21:06 +0100

    feat: show working directory after each operation

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
+import { homedir } from 'os';
+
 const USERNAME_ARG_PREFIX = '--username=';
 
 const startFileManager = () => {
     const args = process.argv.slice(2);
     const enteredUsername = args.find((el) => el.startsWith(USERNAME_ARG_PREFIX))?.slice(USERNAME_ARG_PREFIX.length) || 'Anonymous';
 
+    const workingDirectory = homedir();
+
     console.log(`Welcome to the File Manager, ${enteredUsername}!`);
+
+    console.log(`You are currently in ${workingDirectory}`);
 
     process.stdin.on('data', (data) => {
         const input = data.toString().trim();
         if (input === '.exit') {
             process.exit();
         }
+        //TODO: input handling
+        console.log(`You are currently in ${workingDirectory}`);
+
     });
 
     process.on('SIGINT', () => {
